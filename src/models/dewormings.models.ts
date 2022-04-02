faf74a7199e6b29da8436db4c28ef14f4c134d4d
LeVietHa05 <ha.leviet.0504@gmail.com>
2022-04-02 12:13:13 +0700

    add deworming model

--- src/models/dewormings.models.ts
+++ src/models/dewormings.models.ts
@@ -1,3 +1,19 @@
 /**
  * TODO: Deworming model
  */
+import { model, Schema, Document } from 'mongoose';
+import { dewormings } from '@interfaces/dewormings.interface';
+
+const dewormingSchema: Schema = new Schema<dewormings>({
+  pet_id: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    require: true,
+  },
+});
+
+const dewormingModel = model<dewormings & Document>('Deworming', dewormingSchema);
+export default dewormingModel;
