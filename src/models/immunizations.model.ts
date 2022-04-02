98bcb4ed459843ab38a36205def68d57893783fa
LeVietHa05 <ha.leviet.0504@gmail.com>
2022-04-02 12:32:57 +0700

    add immunization model

--- src/models/immunizations.model.ts
+++ src/models/immunizations.model.ts
@@ -1,3 +1,32 @@
 /**
  * TODO: Immunization model
  */
+import { model, Schema, Document } from 'mongoose';
+import { ImmunizationInterface } from '@interfaces/immunization.interface';
+
+const immunizationsSchema: Schema = new Schema<ImmunizationInterface>({
+  pet_id: {
+    type: String,
+    required: true,
+  },
+  vaccine: {
+    type: Boolean,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  unit: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const immunizationModel = model<ImmunizationInterface & Document>('Immunization', immunizationsSchema);
+
+export default immunizationModel;
