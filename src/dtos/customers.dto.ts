7fcf9791e6f52e52ec4a1ceaa6a367a59b144df4
LeVietHa05 <ha.leviet.0504@gmail.com>
2022-04-04 14:50:47 +0700

    add customer service and customer dto

--- src/dtos/customers.dto.ts
+++ src/dtos/customers.dto.ts
@@ -1,3 +1,18 @@
 /**
  * TODO: Customers Dto
  */
+import { IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreatCustomerDto {
+  @IsString()
+  public name: string;
+
+  @IsPhoneNumber()
+  public phone: string;
+
+  @IsString()
+  public address: string;
+
+  @IsString()
+  public email: string;
+}
--- src/services/customers.service.ts
+++ src/services/customers.service.ts
@@ -1,34 +1,39 @@
 import customerModel from '@models/customers.model';
 import { Customer } from '@interfaces/customers.interface';
+import { HttpException } from '@exceptions/HttpException';
+import { isEmpty } from '@utils/util';
+import { CreatCustomerDto } from '@dtos/customers.dto';
 
 class CustomerService {
   public customerModel = customerModel;
 
-  /**
-   * TODO: Pagination
-   */
-  public async getPagination(page: number, limit = 10) {
-    // Code here
+  public async getPagination(page: number, limit = 10): Promise<Customer[]> {
+    if (page < 0) page = 0;
+    const skip = page * limit;
+    return this.customerModel.find({}, null, { limit, skip }).exec();
   }
 
-  /**
-   * TODO: Get by pet id
-   */
-  public async getById(customerId: string) {
-    // Code here
+  public async getById(customerId: string): Promise<Customer> {
+    if (isEmpty(customerId)) throw new HttpException(400, "You're not customerId");
+    const findCustomer: Customer = await this.customerModel.findOne({ _id: customerId });
+    if (!findCustomer) throw new HttpException(409, "You're not customer");
+    return findCustomer;
   }
 
-  /**
-   * TODO: Create customer info
-   */
-  public async create(info: Customer) {
-    // Code here
+  public async createCustomer(customerData: CreatCustomerDto): Promise<Customer> {
+    if (!isEmpty(customerData)) throw new HttpException(400, "You're not customerData");
+
+    return await this.customerModel.create({ ...customerData });
   }
 
-  /**
-   * TODO: Update customer info
-   */
-  public async update(info: Customer) {
-    // Code here
+  public async updateCustomer(customerId: string, customerData: CreatCustomerDto): Promise<Customer> {
+    if (isEmpty(customerData)) throw new HttpException(400, "You're not customerData");
+
+    const updateCustomerById: Customer = await this.customerModel.findByIdAndUpdate(customerId, { customerData });
+    if (!updateCustomerById) throw new HttpException(409, "You're not customer");
+
+    return updateCustomerById;
   }
 }
+
+export default CustomerService;
