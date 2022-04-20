7fffe82522e1128682b467b22938d11d2716c3b0
LeVietHa05 <ha.leviet.0504@gmail.com>
2022-04-20 21:31:02 +0700

    Ha: add: send form value to server

--- public/js/form/deworming.js
+++ public/js/form/deworming.js
@@ -1,1 +1,13 @@
 // TODO-HA: send form value deworming to server
+window.onload = () => {
+  const petId = location.href.split('/')[4];
+  const submit_btn = document.getElementById('submit-btn');
+
+  submit_btn.onclick = async () => {
+    const date = document.getElementById('deworming-date').value;
+    await axios.post(`/deworming/${petId}`, {
+      date,
+    });
+    location.href = `/pets/${petId}`;
+  };
+};
--- public/js/form/medical-history.js
+++ public/js/form/medical-history.js
@@ -1,1 +1,17 @@
 // TODO-HA: send form value medical history to server
+window.onload = () => {
+  const submit_btn = document.getElementById('submit-btn');
+  const petId = location.href.split('/')[4];
+
+  submit_btn.onclick = async () => {
+    const medHistoryData = ['medical-date', 'medical-note', 'status'];
+    const [date, diagnosis, status] = medHistoryData.map(e => document.getElementById(e).value);
+
+    await axios.post(`/medicalHistories/${petId}`, {
+      date,
+      diagnosis,
+      status,
+    });
+    location.href = `/pets/${petId}`;
+  };
+};
